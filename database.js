95dfd883450bb1fbfc71e724759cf52d2facc266
sriosmar <susana.lizbeth.rios.martinez@everis.com>
2019-02-11 21:42:49 -0300

    database config

--- database.js
+++ database.js
@@ -2,27 +2,26 @@ const mysql = require('mysql');
 
 // Database Connection for Production
 
-// let config = {
-//     user: process.env.SQL_USER,
-//     database: process.env.SQL_DATABASE,
-//     password: process.env.SQL_PASSWORD,
-// }
+ let config = {
+     user: process.env.SQL_USER,
+     database: process.env.SQL_DATABASE,
+     password: process.env.SQL_PASSWORD,
+}
 
-// if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
-//   config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
-// }
+ if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
+   config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+}
 
-// let connection = mysql.createConnection(config);
+let connection = mysql.createConnection(config);
 
 // Database Connection for Development
 
-let connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASS,
-    socketPath :`/cloudsql/${process.env.DB_INSTANCE_NAME}`
-});
+/*let connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    database: process.env.MYSQL_DATABASE,
+    password: process.env.MYSQL_PASS,
+});*/
 
 connection.connect(function(err) {
     if (err) {
